6da8034bde4f22a16cc0aba3f87a1cc56280db93
Diptanshu Kakwani <dipkakwani@gmail.com>
2021-01-21 12:35:22 +0530

    modify applications build process

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,13 +1,36 @@
+@echo off
+
+set argC=0
+for %%x in (%*) do Set /A argC+=1
+
+if NOT %argC%==1 (
+    echo invalid arguments
+    echo please provide vcpkg cmake path
+    EXIT /B 1
+)
+
 if EXIST build-files rmdir /Q /S build-files
 mkdir build-files
 cd build-files
 
+echo Building weak-isolation-mock-db ...
+
 cmake -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE=%1 ../ > log.txt
 
-if errorlevel 1 EXIT /B 1
+if errorlevel 1 (
+    echo BUILD UNSUCCESSFUL
+    echo See build-files/log.txt for details
+    EXIT /B 1
+)
 
-cmake --build ./ --target kv_store_apps > app_log.txt
+echo Build Successful
+echo Building Applications ...
 
-if errorlevel 1 EXIT /B 1
+cmake --build ./ --target courseware_app shopping_cart_app twitter_app stack_app > app_log.txt
 
-ECHO BUILD SUCCESSFUL
+if errorlevel 1 (
+    echo Applications BUILD UNSUCCESSFUL
+    echo See build-files/app_log.txt for details
+    EXIT /B 1
+)
+echo Build Successful
